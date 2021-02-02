d174203efef8a2fa5754330ddc466bb8a0e98b67
Judith Cristina Quiñones Inga <judicriss18@gmail.com>
2021-02-02 13:16:07 -0500

    cambios

--- pages/CategoryAndMarca/[categoria]/[marca].js
+++ pages/CategoryAndMarca/[categoria]/[marca].js
@@ -9,10 +9,31 @@ const CategoryAndMarca = () => {
   const { marca } = useRouter().query;
   console.log(categoria, "judith router");
   console.log(marca, "judith router");
+
+  const getProductsDay = async () => {
+      await clienteAxiosBusinessLocal.get(`/filterCategoryMarca/${categoria}&${marca}`)
+      .then((res) => {
+          if(res.data.MensajeRespuesta === "NO EXISTEN DATOS" ){
+              setfilterCategoriesMarca([]);
+          } else {
+              setfilterCategoriesMarca(res.data.promocionesFilterCategoriaMarca);
+      }
+          // setLoading(false);
+      })
+      .catch((e) => {
+          console.log(e, "error");
+      });
+  };
+
+  useEffect(() => {
+      getProductsDay();
+
+
+  }, [categoria, marca]);
   return (
     <>
       <AppLayout>
-        <p>Hola</p>
+        <p>Hola jojoo</p>
       </AppLayout>
     </>
   );
