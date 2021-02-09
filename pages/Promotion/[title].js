b09f7bb37c48f91fb65867e9e7d9ef0127cbd45a
Judith Cristina Quiñones Inga <judicriss18@gmail.com>
2021-02-08 20:54:08 -0500

    useEffect

--- pages/Promotion/[title].js
+++ pages/Promotion/[title].js
@@ -10,218 +10,247 @@ import Row from "react-bootstrap/Row";
 import { faClock } from "@fortawesome/free-regular-svg-icons";
 import Image from "next/image";
 import { faFacebookF, faTwitter } from "@fortawesome/free-brands-svg-icons";
-// import Head from "next/head";
+import Head from "next/head";
 
 
-const PromotionSpecific = ({ product }) => {
+const PromotionSpecific = () => {
   const [interest, setInterest] = useState(false);
   const [btnShare, setBtnShare] = useState(false);
   const router = useRouter();
   console.log(router, "hola soy la ruta");
+  const [product, setProducts] = useState([]);
   const functionShare = () => {
     setBtnShare(!btnShare);
   };
+
+  // console.log(localStorage.getItem("searchFilterLocalStorage"));
+  // console.log(router, "pruebita");
+  const getProducts = async () => {
+    let url = `${process.env.NEXT_PUBLIC_REACT_APP_BACKEND_URL_BUSINESS_LOCAL}/get-promotion/${router.query.id}`;
+    await fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        if (data.MensajeRespuesta === "NO EXISTEN DATOS") {
+          setProducts([]);
+        } else {
+          console.log(data,"ojitos")
+          setProducts(data[0]);
+        }
+      })
+      .catch((e) => {
+        console.log(e, "error:)");
+      });
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, [router.query.id]);
   return (
     <>
-      <AppLayout>
-        {/* <Head>
-          <title>{product.promocion.nombre}</title>
-          <meta
-            name="description"
-            content={product.promocion.descripcion}
-          ></meta>
-          <meta property="og:type" content="website" />
-          <meta property="og:title" content={product.promocion.nombre} />
-          <meta
-            property="og:description"
-            content={product.promocion.descripcion}
-          />
-          <meta
-            property="og:image"
-            content={
-              product.imagenes[1].typeImage === "O"
-                ? product.imagenes[1].url
-                : product.imagenes[0].url
-            }
-          />
-           <meta property="og:site_name" content="La Ganga" />
-        </Head> */}
+     {
+       product.length===0 ? "" :
+         <AppLayout>
+          <Head>
+            <title>{product.promocion.nombre}</title>
+            <meta
+              name="description"
+              content={product.promocion.descripcion}
+            ></meta>
+            <meta property="og:type" content="website" />
+            <meta property="og:title" content={product.promocion.nombre} />
+            <meta
+              property="og:description"
+              content={product.promocion.descripcion}
+            />
+            <meta
+              property="og:image"
+              content={
+                product.imagenes[1].typeImage === "O"
+                  ? product.imagenes[1].url
+                  : product.imagenes[0].url
+              }
+            />
+             <meta property="og:site_name" content="La Ganga" />
+          </Head>
 
-        <Container className="pt-4 box-home container-ganga fade-in animated">
-          <h4 className="title-ganga title-especific">Ver mas detalles:</h4>
-          <div className="box-details margin-box">
-            <figure className="figure-product-especific">
-              <img
-                src={
-                  product.imagenes[1].typeImage === "O"
-                    ? product.imagenes[1].url
-                    : product.imagenes[0].url
-                }
-                alt=""
-                className="w-100"
-              />
-              <div className="grup-btn-especific">
-                <div className="btn-cirle-especific" onClick={functionShare}>
-                  <FontAwesomeIcon
-                    icon={faShareAlt}
-                    className="btn-share-especific"
-                  />
+          <Container className="pt-4 box-home container-ganga fade-in animated">
+            <h4 className="title-ganga title-especific">Ver mas detalles:</h4>
+            <div className="box-details margin-box">
+              <figure className="figure-product-especific">
+                <img
+                  src={
+                    product.imagenes[1].typeImage === "O"
+                      ? product.imagenes[1].url
+                      : product.imagenes[0].url
+                  }
+                  alt=""
+                  className="w-100"
+                />
+                <div className="grup-btn-especific">
+                  <div className="btn-cirle-especific" onClick={functionShare}>
+                    <FontAwesomeIcon
+                      icon={faShareAlt}
+                      className="btn-share-especific"
+                    />
+                  </div>
+                  {btnShare === false ? (
+                    ""
+                  ) : (
+                    <div className="socialGallery">
+                      <div class="socialToolBox">
+                        <a
+                          href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Flocalhost%3A3000%2FPromotion%2Ft28%3Fid%3D5fea41b90e52d344743bf74f&amp;src=sdkpreparse"
+                          className="btn-cirle-especific"
+                          target="_blank"
+                        >
+                          <FontAwesomeIcon
+                            icon={faFacebookF}
+                            className="btn-share-especific"
+                          />
+                        </a>
+                        <a className="btn-cirle-especific">
+                          <FontAwesomeIcon
+                            icon={faTwitter}
+                            className="btn-share-especific"
+                          />
+                        </a>
+                      </div>
+                    </div>
+                  )}
+                  <div className="btn-cirle-especific">
+                    <FontAwesomeIcon
+                      icon={faHeart}
+                      className={
+                        interest
+                          ? "btn-like-especific-active"
+                          : "btn-like-especific"
+                      }
+                      // onClick={addInterestEspecific}
+                    />
+                  </div>
                 </div>
-                {btnShare === false ? (
-                  ""
-                ) : (
-                  <div className="socialGallery">
-                    <div class="socialToolBox">
-                      <a
-                        href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Flocalhost%3A3000%2FPromotion%2Ft28%3Fid%3D5fea41b90e52d344743bf74f&amp;src=sdkpreparse"
-                        className="btn-cirle-especific"
-                        target="_blank"
+                {product.promocion.tipoDescuento === "freeShipping" ? (
+                  <div className="box-tag-offer-especific">
+                    <div className="box-free-shipping">
+                      <div
+                        className={
+                          "letter-free" +
+                          " " +
+                          "letter-free-cl-" +
+                          product.promocion.categoria
+                        }
                       >
-                        <FontAwesomeIcon
-                          icon={faFacebookF}
-                          className="btn-share-especific"
-                        />
-                      </a>
-                      <a className="btn-cirle-especific">
-                        <FontAwesomeIcon
-                          icon={faTwitter}
-                          className="btn-share-especific"
-                        />
-                      </a>
+                        free
+                      </div>
+                      <div
+                        className={
+                          "box-letter-free" +
+                          " " +
+                          "box-letter-free-bg-" +
+                          product.promocion.categoria
+                        }
+                      >
+                        shipping
+                      </div>
+                    </div>
+                  </div>
+                ) : product.promocion.tipoDescuento === "2x1" ? (
+                  <div className="box-tag-offer-especific">
+                    <div
+                      className={
+                        "box-x" + " " + "box-x-" + product.promocion.categoria
+                      }
+                    >
+                      2x1
+                    </div>
+                  </div>
+                ) : product.promocion.tipoDescuento === "3x2" ? (
+                  <div className="box-tag-offer-especific">
+                    <div
+                      className={
+                        "box-x" + " " + "box-x-" + product.promocion.categoria
+                      }
+                    >
+                      3x2
+                    </div>
+                  </div>
+                ) : product.promocion.descuentoOtros === "otros" ? (
+                  <div className="box-tag-offer-especific">
+                    <div
+                      className={
+                        "box-x" + " " + "box-x-" + product.promocion.categoria
+                      }
+                    >
+                      {product.promocion.descuentoOtros}
+                    </div>
+                  </div>
+                ) : (
+                  <div className="box-tag-offer-especific">
+                    <div
+                      className={
+                        "box-porcent" +
+                        " " +
+                        "box-porcent-" +
+                        product.promocion.categoria
+                      }
+                    >
+                      -{product.promocion.descuento}%
                     </div>
                   </div>
                 )}
-                <div className="btn-cirle-especific">
-                  <FontAwesomeIcon
-                    icon={faHeart}
-                    className={
-                      interest
-                        ? "btn-like-especific-active"
-                        : "btn-like-especific"
-                    }
-                    // onClick={addInterestEspecific}
-                  />
+                <img
+                  src={
+                    product.imagenes[1].typeImage === "M"
+                      ? product.imagenes[1].url
+                      : product.imagenes[0].url
+                  }
+                  alt=""
+                  className="marca-especific"
+                />
+              </figure>
+              <div></div>
+              <div className="box-details-especific">
+                <h3 className="title-ganga subtitle-especific">
+                  {product.promocion.nombre}
+                </h3>
+                <div className="group-time">
+                  <FontAwesomeIcon icon={faClock} className="img-clock" />
+                  <div className="ml-2">5D 4h 1M</div>
                 </div>
+                <div className="description-especific">
+                  {product.promocion.descripcion}
+                  {product.promocion.categoria}
+                </div>
+                <Button className="btn-ir-web">Ir a la web</Button>
               </div>
-              {product.promocion.tipoDescuento === "freeShipping" ? (
-                <div className="box-tag-offer-especific">
-                  <div className="box-free-shipping">
-                    <div
-                      className={
-                        "letter-free" +
-                        " " +
-                        "letter-free-cl-" +
-                        product.promocion.categoria
-                      }
-                    >
-                      free
-                    </div>
-                    <div
-                      className={
-                        "box-letter-free" +
-                        " " +
-                        "box-letter-free-bg-" +
-                        product.promocion.categoria
-                      }
-                    >
-                      shipping
-                    </div>
-                  </div>
-                </div>
-              ) : product.promocion.tipoDescuento === "2x1" ? (
-                <div className="box-tag-offer-especific">
-                  <div
-                    className={
-                      "box-x" + " " + "box-x-" + product.promocion.categoria
-                    }
-                  >
-                    2x1
-                  </div>
-                </div>
-              ) : product.promocion.tipoDescuento === "3x2" ? (
-                <div className="box-tag-offer-especific">
-                  <div
-                    className={
-                      "box-x" + " " + "box-x-" + product.promocion.categoria
-                    }
-                  >
-                    3x2
-                  </div>
-                </div>
-              ) : product.promocion.descuentoOtros === "otros" ? (
-                <div className="box-tag-offer-especific">
-                  <div
-                    className={
-                      "box-x" + " " + "box-x-" + product.promocion.categoria
-                    }
-                  >
-                    {product.promocion.descuentoOtros}
-                  </div>
-                </div>
-              ) : (
-                <div className="box-tag-offer-especific">
-                  <div
-                    className={
-                      "box-porcent" +
-                      " " +
-                      "box-porcent-" +
-                      product.promocion.categoria
-                    }
-                  >
-                    -{product.promocion.descuento}%
-                  </div>
-                </div>
-              )}
-              <img
-                src={
-                  product.imagenes[1].typeImage === "M"
-                    ? product.imagenes[1].url
-                    : product.imagenes[0].url
-                }
-                alt=""
-                className="marca-especific"
+            </div>
+            <div>
+              <div className="box-gangaDelDia margin-box">
+                <h1 className="title-producto-similar">Productos similares</h1>
+                {/* <PromoSimilar addInterest={addInterest} products={products} category={product.promocion.categoria} /> */}
+              </div>
+              <Row className="justify-content-md-center ">
+                {/*<Item />
+               <Item />
+               <Item />
+               <Item />*/}
+              </Row>
+            </div>
+            <figure className="m-0 w-100">
+              <Image
+                src="/images/banner/banner-bottom.png"
+                alt="banner"
+                // layout="fill"
+                width={1500}
+                height={400}
+                //   layout="responsive"
               />
             </figure>
-            <div></div>
-            <div className="box-details-especific">
-              <h3 className="title-ganga subtitle-especific">
-                {product.promocion.nombre}
-              </h3>
-              <div className="group-time">
-                <FontAwesomeIcon icon={faClock} className="img-clock" />
-                <div className="ml-2">5D 4h 1M</div>
-              </div>
-              <div className="description-especific">
-                {product.promocion.descripcion}
-                {product.promocion.categoria}
-              </div>
-              <Button className="btn-ir-web">Ir a la web</Button>
-            </div>
-          </div>
-          <div>
-            <div className="box-gangaDelDia margin-box">
-              <h1 className="title-producto-similar">Productos similares</h1>
-              {/* <PromoSimilar addInterest={addInterest} products={products} category={product.promocion.categoria} /> */}
-            </div>
-            <Row className="justify-content-md-center ">
-              {/*<Item />
-             <Item />
-             <Item />
-             <Item />*/}
-            </Row>
-          </div>
-          <figure className="m-0 w-100">
-            <Image
-              src="/images/banner/banner-bottom.png"
-              alt="banner"
-              // layout="fill"
-              width={1500}
-              height={400}
-              //   layout="responsive"
-            />
-          </figure>
-        </Container>
-      </AppLayout>
+          </Container>
+        </AppLayout>
+     }
       <style jsx>
         {`
           .socialGallery {
@@ -385,27 +414,27 @@ const PromotionSpecific = ({ product }) => {
   );
 };
 
-export async function getServerSideProps(params) {
-  // Call an external API endpoint to get posts.
-  // You can use any data fetching library
-  const routerParams = params;
-  let url = `${process.env.NEXT_PUBLIC_REACT_APP_BACKEND_URL_BUSINESS_LOCAL}/get-promotion/${routerParams.query.id}`;
-  const res = await fetch(url);
+// export async function getServerSideProps(params) {
+//   // Call an external API endpoint to get posts.
+//   // You can use any data fetching library
+//   const routerParams = params;
+//   let url = `${process.env.NEXT_PUBLIC_REACT_APP_BACKEND_URL_BUSINESS_LOCAL}/get-promotion/${routerParams.query.id}`;
+//   const res = await fetch(url);
 
-  const product = await res.json();
-  if (!product) {
-    return {
-      notFound: true,
-    };
-  }
+//   const product = await res.json();
+//   if (!product) {
+//     return {
+//       notFound: true,
+//     };
+//   }
 
-  // By returning { props: posts }, the Blog component
-  // will receive `posts` as a prop at build time
-  return {
-    props: {
-      product: product[0],
-    },
-  };
-}
+//   // By returning { props: posts }, the Blog component
+//   // will receive `posts` as a prop at build time
+//   return {
+//     props: {
+//       product: product[0],
+//     },
+//   };
+// }
 
 export default PromotionSpecific;
