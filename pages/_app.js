d366f2f65474e726bd1d1907b79199a4dac727ec
Judith Cristina Quiñones Inga <judicriss18@gmail.com>
2021-02-23 18:35:17 -0500

    fuente html

--- pages/_app.js
+++ pages/_app.js
@@ -148,15 +148,19 @@ function MyApp({ Component, pageProps }) {
             font-family: "made-tomy-sof-regular";
             src: url(fonts/made-tommy.otf);
           }
+          html {
+            font-family: "made-tomy-sof-regular";
+          }
 
-          body{
+          body {
             font-family: "made-tomy-sof-regular";
           }
           .font-made-tomy-sof-regular {
             font-family: "made-tomy-sof-regular";
           }
 
-          {/* .title-ganga {
+           {
+            /* .title-ganga {
             font-family: "made-tomy-sof-regular";
             font-size: 1.8rem;
             font-weight: 700;
@@ -170,7 +174,8 @@ function MyApp({ Component, pageProps }) {
 
           .link {
             text-decoration: none !important;
-          } */}
+          } */
+          }
 
            {
             /* .box-home{
