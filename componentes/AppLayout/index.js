db962a9237927abb74cc0f2acdbbb3f07ddfe800
Judith Cristina Quiñones Inga <judicriss18@gmail.com>
2021-02-26 13:19:54 -0500

    estilos

--- componentes/AppLayout/index.js
+++ componentes/AppLayout/index.js
@@ -45,120 +45,6 @@ const AppLayout = ({ children, promotionsData }) => {
       </div>
       <style jsx global>
         {`
-          @font-face {
-            font-family: "made-tomy-sof-regular";
-            src: url(fonts/made-tommy.otf);
-          }
-          html {
-            font-family: "made-tomy-sof-regular";
-          }
-
-          body {
-            font-family: "made-tomy-sof-regular";
-          }
-          .font-made-tomy-sof-regular {
-            font-family: "made-tomy-sof-regular";
-          }
-
-          /* Estilos Judith  :D */
-
-          .container,
-          .container-fluid,
-          .container-lg,
-          .container-md,
-          .container-sm,
-          .container-xl {
-            width: 100%;
-            padding-right: 0px !important;
-            padding-left: 0px !important;
-          }
-
-          .margin-box {
-            padding: 0rem 5rem !important;
-          }
-
-          .carousel-control-next,
-          .carousel-control-prev {
-            width: 8% !important;
-          }
-
-          @media (min-width: 576px) {
-            .container-ganga {
-              max-width: 900px !important;
-              margin: auto;
-            }
-          }
-          @media (min-width: 900px) {
-            .container-ganga {
-              max-width: 992px !important;
-              margin: auto;
-            }
-          }
-          @media (min-width: 992px) {
-            .container-ganga {
-              max-width: 1200px !important;
-              margin: auto;
-            }
-          }
-          @media (min-width: 1200px) {
-            .container-ganga {
-              max-width: 1800px !important;
-              margin: auto;
-            }
-          }
-
-          @media (max-width: 769px) {
-            html {
-              font-size: 4.6vw;
-            }
-            .margin-box {
-              padding: 0rem 1rem !important;
-            }
-
-            .title-ganga {
-              font-size: 1.4rem;
-            }
-
-            .subtitle-ganga {
-              font-size: 0.8rem;
-            }
-          }
-
-          /* ANIMACIONES */
-          .react-parallax-bgimage {
-            position: fixed !important;
-            height: 100% !important;
-            width: auto !important;
-          }
-
-          .fade-in {
-            -webkit-animation: fade-in 1.2s cubic-bezier(0.39, 0.575, 0.565, 1)
-              both;
-            animation: fade-in 1.2s cubic-bezier(0.39, 0.575, 0.565, 1) both;
-          }
-          @-webkit-keyframes fade-in {
-            0% {
-              opacity: 0;
-            }
-            100% {
-              opacity: 1;
-            }
-          }
-          @keyframes fade-in {
-            0% {
-              opacity: 0;
-            }
-            100% {
-              opacity: 1;
-            }
-          }
-
-          .animated {
-            -webkit-animation-duration: 3s;
-            animation-duration: 3s;
-            -webkit-animation-fill-mode: both;
-            animation-fill-mode: both;
-          }
 
           .margin-box {
             padding: 0rem 5rem !important;
@@ -329,6 +215,7 @@ const AppLayout = ({ children, promotionsData }) => {
             color: #7e4fff;
           }
 
+
           .box-otro {
             border-radius: 6%;
             padding: 0.3rem 0.5rem;
@@ -432,6 +319,8 @@ const AppLayout = ({ children, promotionsData }) => {
               bottom: 0%;
             }
           }
+
+
         `}
       </style>
     </>
